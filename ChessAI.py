b40c6f3bdc444b533eb3d8e1ad21a362880d6f2d
curtisbucher <cpbucher5@gmail.com>
2019-01-12 21:50:45 -0800

    Added recursive scoring function

--- ChessAI.py
+++ ChessAI.py
@@ -17,6 +17,8 @@ def gen_possible_moves(board, black = True):
     return legal_moves
 
 def score(board, score_move, black = True,):
+    """ Scores the curret move based on how many peices are on the board after the move is executed"""
+    
     temp_board = ChessBoard.chessboard(board)
     start, end = tuple(score_move)
     temp_board.move_piece(start, end)
@@ -32,11 +34,74 @@ def score(board, score_move, black = True,):
                 else:
                     total_score -= scores[piece.lower()]
     temp_board.move_piece(end, start)
-    return total_score         
-            
-                        
+    return total_score
+
+def recur_score_move(depth, board, black=True, curr_depth = 0):
+    """ Scores the board by recursing `depth` moves deep. Prunes tree based by only taking the best half
+    of moves"""
+    
+    score = 0
+    
+    ## Finding all the legal moves for the computer
+    moves = gen_possible_moves(board, black)
+    
+    ## Should prune here, waiting untill later
+    moves = moves[0:2]
+    
+    if curr_depth < depth:
+        ## Runs if the maximum recursion depth is not reached
+        
+        ## Scoring all the moves
+        for move in moves: 
+            ## Creating a new board, where the move was executed
+            new_board = ChessBoard.chessboard(board)
+            ## Scoring the move
+            score += recur_score_move(depth, new_board, black= black, curr_depth = curr_depth + 1)
+        
+    ## returning score to collapse tree if not root
+    ##print(curr_depth)
+    
+    ## If computer's turn to move, curr_depth will be an odd number and score will be returned positive
+    if curr_depth%2:
+        return score
+    ## If player's turn to move, curr_depth will be an even numver and score will be returned negetive
+    else:
+        return -score
+    
+def best_move(board, depth, black=True):
+    """ Scores all the possible moves for the computer make by calling recur_score_move() on each move"""
+    moves = gen_possible_moves(board, black)
+    
+    ## For viewing processing time
+    print("Legal Moves: " + str(len(moves)))
+    print("-"*len(moves))
+        
+    
+    for move in moves: 
+        ## Creating a new board, where the move was executed
+        new_board = ChessBoard.chessboard(board)
+        ## Executing and scoreing move
+        new_board.move_piece(move.start_coords, move.end_coords)
+        move.score = score(board, move, black)
+        
+        ## Recursively scoring next gen possible moves
+        move.score += recur_score_move(depth, new_board, black= black, curr_depth = 0)
+        print("#",end="")
+    print()
+    
+    ## Creating new, blank move with default score to be compared to max_move
+    max_move = moves[0]
+     
+    ## Finding the highest scoring move
+    for move in moves:
+        if move.score > max_move.score:
+            max_move = move
+     
+    return max_move
+    
 ## Player is white
 while True:
+    
     ChessBoard.draw_board(board, True)
     
     user_move = ChessBoard.get_move(board,False)
@@ -46,20 +111,22 @@ while True:
         user_move = ChessBoard.get_move(board,False)
     user_move.execute()
 
-    max_move = random.choice(gen_possible_moves(board))
-
-    ## Scoring all the moves
-    max_score = -math.inf
-    max_move = gen_possible_moves(board)[0]
-    for score_move in gen_possible_moves(board):
-        move_score = score(board, score_move,True)
-        if move_score > max_score:
-            max_score = move_score
-            max_move = score_move
-            
+#    max_move = random.choice(gen_possible_moves(board))
+#
+#    ## Scoring all the moves
+#    max_score = -math.inf
+#    max_move = gen_possible_moves(board)[0]
+#    for score_move in gen_possible_moves(board):
+#        move_score = score(board, score_move,True)
+#        if move_score > max_score:
+#            max_score = move_score
+#            max_move = score_move
+    
+    max_move = best_move(board, 4, True)
     max_move.execute()
+
     print("<<<",end=" ")
     print("Move: " + str(max_move))
-    print("    Score: " + str(max_score))
+    print("    Score: " + str(max_move.score))
 
 
