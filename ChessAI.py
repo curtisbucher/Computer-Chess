312f2684e08d1d35f33b71c537686a37df0cea2f
curtisbucher <cpbucher5@gmail.com>
2019-12-25 15:18:18 -0800

    eliminated nested for loops

--- ChessAI.py
+++ ChessAI.py
@@ -6,7 +6,7 @@ from functools import lru_cache
 import itertools
 
 board = ChessBoard.chessboard()
-## Variables for pruning
+# Variables for pruning
 DEPTH = 4
 PRUNE = 3
 
@@ -15,10 +15,11 @@ def gen_possible_moves(board, black=True):
     """ Goes through every single move on the board, finds which ones are
     legal, then returns a list of move objects that are legal"""
     legal_moves = []
-    ## Iterating through every possible move on the board
+    # Iterating through every possible move on the board
     for (sx, sy, ex, ey) in itertools.product(range(8), range(8), range(8), range(8)):
         if ChessBoard.legal_move(board, (sx, sy), (ex, ey), black):
-            legal_moves.append(ChessBoard.move(board, (sx, sy), (ex, ey), black))
+            legal_moves.append(ChessBoard.move(
+                board, (sx, sy), (ex, ey), black))
     return legal_moves
 
 
@@ -29,18 +30,17 @@ def score(board, score_move, black=True):
     start, end = tuple(score_move)
     temp_board.move_piece(start, end)
 
-    offensive_scores = {"p": 1, "r": 5, "h": 3, "b": 3, "q": 9, "k": 130}  ##
-    defensive_scores = {"p": 1, "r": 5, "h": 3, "b": 3, "q": 9, "k": 40}  ##
+    offensive_scores = {"p": 1, "r": 5, "h": 3, "b": 3, "q": 9, "k": 130}
+    defensive_scores = {"p": 1, "r": 5, "h": 3, "b": 3, "q": 9, "k": 40}
 
     total_score = 0
-    for x in range(8):
-        for y in range(8):
-            piece = temp_board.pieces[y][x]
-            if piece != " ":
-                if piece.isupper() == black:  ## upper and black or lower and not black
-                    total_score += offensive_scores[piece.lower()]  ##
-                else:
-                    total_score -= defensive_scores[piece.lower()]  ##
+    for (x, y) in itertools.product(range(8), range(8)):
+        piece = temp_board.pieces[y][x]
+        if piece != " ":
+            if piece.isupper() == black:  # upper and black or lower and not black
+                total_score += offensive_scores[piece.lower()]
+            else:
+                total_score -= defensive_scores[piece.lower()]
     temp_board.move_piece(end, start)
     return total_score
 
@@ -51,85 +51,79 @@ def recur_score_move(depth, board, black=True, curr_depth=0):
 
     tot_score = 0
 
-    ## Finding all the legal moves for the computer
+    # Finding all the legal moves for the computer
     moves = gen_possible_moves(board, black)
 
-    ## Pruning, finding the best `number` of moves
+    # Pruning, finding the best `number` of moves
 
-    ##Scoring
+    # Scoring
     for move in moves:
         move.score = score(board, move, black)
         tot_score += move.score
 
-    ## Sorting by score
+    # Sorting by score
     sorted_moves = sorted(moves, key=lambda x: x.score, reverse=True)
     moves = sorted_moves[0:PRUNE]
 
     if curr_depth < depth:
-        ## Runs if the maximum recursion depth is not reached
+        # Runs if the maximum recursion depth is not reached
 
-        ## Scoring all the moves
+        # Scoring all the moves
         for move in moves:
-            ## Creating a new board, where the move was executed
+            # Creating a new board, where the move was executed
             new_board = ChessBoard.chessboard(board)
             new_board.move_piece(move.start_coords, move.end_coords)
 
-            ## Scoring the move by adding all its own possible moves and subtracting the others. Deals with averages
-<<<<<<< HEAD
+            # Scoring the move by adding all its own possible moves and subtracting the others. Deals with averages
             tot_score -= recur_score_move(
                 depth, new_board, black=not black, curr_depth=curr_depth + 1
             ) / (
                 PRUNE
-            )  ## ** depth)
+            )  # ** depth)
+    # returning score to collapse tree if not root
+    # print(curr_depth)
 
-=======
-            tot_score -= recur_score_move(depth, new_board, black = not black, curr_depth = curr_depth + 1)/(3**depth)
-        
->>>>>>> c93a670cb0393e3e9ce55e77f231ca14c2f8f8ef
-    ## returning score to collapse tree if not root
-    ##print(curr_depth)
-
-    ## If computer's turn to move, curr_depth will be an odd number and score will be returned positive
-    ##if curr_depth%2:
+    # If computer's turn to move, curr_depth will be an odd number and score will be returned positive
+    # if curr_depth%2:
     # os.system('cls')
     # ChessBoard.draw_board(board, True)
     return tot_score
 
 
-## If player's turn to move, curr_depth will be an even numver and score will be returned negetive
-##else:
-##return -score
+# If player's turn to move, curr_depth will be an even numver and score will be returned negetive
+# else:
+# return -score
 
 
 def best_move(board, depth, black=True):
     """ Scores all the possible moves for the computer make by calling recur_score_move() on each move"""
     moves = gen_possible_moves(board, black)
 
-    ## For viewing processing time
+    # For viewing processing time
     print("Legal Moves: " + str(len(moves)))
     print("-" * len(moves))
 
     for move in moves:
         print("#", end="", flush=True)
-        ## Creating a new board, where the move was executed
+        # Creating a new board, where the move was executed
         new_board = ChessBoard.chessboard(board)
-        ## Executing and scoreing move`
+        # Executing and scoreing move`
         new_board.move_piece(move.start_coords, move.end_coords)
         move.score = score(board, move, black)
 
-        ## Recursively scoring next gen possible moves
+        # Recursively scoring next gen possible moves
         move.score -= recur_score_move(
             depth, new_board, black=not black, curr_depth=0
         ) / (
             PRUNE
-        )  ## ** depth)
+        )  # ** depth)
 
     print()
 
-    ## Creating new, blank move with default score to be compared to max_move
+    # Creating new, blank move with default score to be compared to max_move
     max_move = moves[0]
 
-    ## Finding the highest scoring move
+    # Finding the highest scoring move
     for move in moves:
         if move.score > max_move.score:
             max_move = move
@@ -148,24 +142,26 @@ def player_v_CPU():
             user_move = ChessBoard.get_move(board, False)
         user_move.execute()
 
-        ## Testing for King in check
+        # Testing for King in check
         is_check, is_black = ChessBoard.check(board)
         if is_check:
             print(
-                ("Black" * is_black) + ("White" * (not is_black)) + " king is in check!"
+                ("Black" * is_black) + ("White" *
+                                        (not is_black)) + " king is in check!"
             )
 
         max_move = best_move(board, DEPTH, True)
         max_move.execute()
 
-        ## Testing for King in check
+        # Testing for King in check
         is_check, is_black = ChessBoard.check(board)
         if is_check:
             print(
-                ("Black" * is_black) + ("White" * (not is_black)) + " king is in check!"
+                ("Black" * is_black) + ("White" *
+                                        (not is_black)) + " king is in check!"
             )
 
-        ## Ascii bell tone
+        # Ascii bell tone
         print("\a")
 
         print("<<<", end=" ")
@@ -192,9 +188,9 @@ def CPU_v_CPU():
         print("Move: " + str(B_max_move))
         print("    Score: " + str(B_max_move.score))
 
-        ## Ascii bell tone
+        # Ascii bell tone
         print("\a")
 
 
-##CPU_v_CPU()
+# CPU_v_CPU()
 player_v_CPU()
